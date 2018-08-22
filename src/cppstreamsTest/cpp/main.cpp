cc2052b9a978cfcd44c6fa8e1225cbf895a23ce8
BrainStone <yannick@tedworld.de>
2018-08-22 22:57:44 +0200

    Added License headers to all files

--- src/cppstreamsTest/cpp/main.cpp
+++ src/cppstreamsTest/cpp/main.cpp
@@ -1,3 +1,8 @@
+//
+// Copyright (c) 2016-2018 Fernando Damian Lopez
+// Copyright (c) 2018 Yannick Schinko
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
 #include <iostream>
 #include <list>
 #include "../../src/cppstreams.cpp"
