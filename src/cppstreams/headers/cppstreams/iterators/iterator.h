e7a8ae818ab4ab5cff84d8ba04822658266adceb
BrainStone <yannick@tedworld.de>
2018-08-23 23:34:04 +0200

    Implemented iterators::iterator<T>

--- src/cppstreams/headers/cppstreams/iterators/iterator.h
+++ src/cppstreams/headers/cppstreams/iterators/iterator.h
@@ -14,7 +14,29 @@ namespace cppstreams {
 		 * To support more containers, we just need to add more subclasses.
 		 */
 		template<class T>
-		class iterator {};
+		class iterator {
+		public:
+			/**
+			 * Returns the next value from the underlying container.
+			 *
+			 * The method is supposed to first fetch the value the iterator is currently pointing at and then
+			 * advance the pointer to the next object.
+			 *
+			 * <strong>WARNING</strong>:
+			 * Undefined behavior if <code>hasNext()</code> returns <code>false</code>!<br>
+			 * Make sure to always check that before calling this method!
+			 */
+			virtual const T next() = 0;
+
+			/**
+			 * Checks if there is a next value to return.
+			 *
+			 * This method doesn't change the internal state of the iterator and is safe to call.<br>
+			 * <strong>ALWAYS</strong> call this before <code>next()</code>. (And only call this if this method
+			 * returns true!
+			 */
+			virtual bool hasNext() const noexcept = 0;
+		};
 	}
 }
 
